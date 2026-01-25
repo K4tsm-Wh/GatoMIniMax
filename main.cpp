f19f8ad6098e9d81e1e71b09bb364d38febc84f8
Jossette12 <jossette.doll@alumnos.ucn.cl>
2026-01-25 20:12:08 -0300

    Implement winner verification function
    Add function to check for a winner in the game.

--- main.cpp
+++ main.cpp
@@ -44,6 +44,24 @@ struct Gato
     }
 };
 
+char verificarGanador(){
+
+        for (int i = 0; i <  3; i++) {
+
+            if (tablero[i][0] != ' ' && tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2] ){
+                return tablero[i][0];}
+
+            if (tablero[0][i] != ' ' && tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i] ){
+                return tablero[0][i];}}
+
+        if (tablero[0][0] != ' ' && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]){
+            return tablero[0][0];}
+
+        if (tablero[0][2] != ' ' && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]){
+            return tablero[0][2];}
+
+    return ' ';}
+
 main()
 {
     Gato gato;
