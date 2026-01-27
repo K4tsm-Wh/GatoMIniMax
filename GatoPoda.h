70949adde7b175550ca5f04032c9bb005921e0a4
Jossette12 <jossette.doll@alumnos.ucn.cl>
2026-01-26 21:44:39 -0300

    Add GatoPoda class for Tic-Tac-Toe game

--- GatoPoda.h
+++ GatoPoda.h
@@ -1,1 +1,24 @@
 
+#pragma once
+#include <iostream>
+using namespace std;
+
+class GatoPoda
+{
+private:
+    char tablero[3][3];
+    char jugadorActual;
+
+public:
+    GatoPoda(); 
+    void inicializar();
+    void dibujar();
+    bool mover(int fila, int col);
+    char verificarGanador();
+    bool hayEmpate();
+    int evaluarLinea(char a, char b, char c);
+    int revisarPuntaje();
+    int minimax(int profundidad, bool esMaximizar, int alpha, int beta);
+    void jugarIA();
+    char getJugadorActual();
+};
